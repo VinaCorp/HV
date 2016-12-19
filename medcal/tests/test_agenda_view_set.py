77093d2b821d3c6e6ecdcf7d5a374d4321405599
Ramiro Batista da Luz <ramiroluz@gmail.com>
2016-12-19 14:24:58 -0200

    Add the PUT method to agenda's update.

--- medcal/tests/test_agenda_view_set.py
+++ medcal/tests/test_agenda_view_set.py
@@ -14,20 +14,25 @@ class AgendaViewSetList(APITestCase):
         especialidade = Especialidade.objects.create(
             nome="Acupuntura"
         )
+
         medico = Medico.objects.create(
             nome="Amauri Jose Zuotoski",
             especialidade=especialidade
         )
+
         paciente = Paciente.objects.create(
             nome="Bernaldo Feigundes"
         )
-        agenda = Agenda.objects.create(
+
+        self.agenda = Agenda.objects.create(
             medico=medico,
             datahora=timezone.datetime(
                 2017, 5, 12, 16, 30, tzinfo=timezone.get_current_timezone()
             ),
             paciente=paciente
         )
+
+        self.agenda.save()
 
         self.obj = [
             {
@@ -50,3 +55,30 @@ class AgendaViewSetList(APITestCase):
         response = self.client.get('/apiv0/agenda/', {'search': 'Acupuntura'})
         self.assertTrue(status.is_success(response.status_code))
         self.assertEqual(response.data, self.obj)
+
+    def test_update(self):
+        """
+        Ensure we can update an object.
+        """
+        url = r('api:agenda-list')
+        # Clear the scheduled patient.
+        self.agenda.paciente = None
+        self.agenda.save()
+        pk = self.agenda.id
+        self.assertTrue(Agenda.objects.get(pk=pk).paciente is None)
+
+        data = {
+            "medico": self.agenda.medico.id,
+            "paciente": 1,
+            "datahora": self.agenda.datahora,
+        }
+
+        url_pk = '{}{}/'.format(url, pk)
+        # response = self.client.put(url_pk, data, format='json')
+        response = self.client.put(url_pk, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Agenda.objects.count(), 1)
+        agenda_obj = Agenda.objects.get(pk=pk)
+        self.assertEqual(agenda_obj.paciente.id, 1)
+        self.assertEqual(agenda_obj.medico.id, 1)
+        self.assertEqual(agenda_obj.datahora, self.agenda.datahora)
